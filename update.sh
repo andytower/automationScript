b742f39a6e403276a6f8a9028ad2cbcd08ccb1c6
Andrey Alferov <alferov@andytower.ru>
2018-09-19 16:03:57 +0300

    Add script for upgrade Unubtu

---
+++ update.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+# This script auto upgrade system Ubuntu
+
+sudo apt update && sudo apt list --upgradable
+echo 'Do you want upgrade? (y/n)'
+read answer
+
+if [[ $answer =~ [yY] ]] 
+then
+  yes Y | sudo apt upgrade 
+fi	
