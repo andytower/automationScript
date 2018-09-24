5e83345dbe42388211fa24e5c627fae5e82c564f
Andrey Alferov <alferov@andytower.ru>
2018-09-24 15:46:30 +0300

    Correct error in script

--- backup.sh
+++ backup.sh
@@ -1,8 +1,9 @@
-#!/etc/bash
+#!/bin/bash
 # This is simple backup home dir to Yandex.Disk
 # It's need yandex-disk client
-
-tar -czf /backup/andytower_home.tar.gz /home/
+rsync -azRv --exclude /home/andytower/Downloads/ --exclude /home/andytower/Yandex.Disk/ --exclude /home/andytower/Загрузки/ /home/andytower/ /backup/ > /backup/backup.log
+cd /backup
+tar -czf /backup/andytower_home.tar.gz home/
 cp /backup/andytower_home.tar.gz ~/Yandex.Disk/Backup
 rm /backup/andytower_home.tar.gz
 yandex-disk sync
