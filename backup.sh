1a8fee3996cfa7e7225192a8565e421d6cd08844
Andrey Alferov <alferov@andytower.ru>
2018-12-02 12:39:34 +0300

    Add creating list of installed package

--- backup.sh
+++ backup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # This is simple backup home dir to Yandex.Disk
 # It's need yandex-disk client
+apt list --installed > ~/pkg_installed.lst 
 rsync -aRv --exclude /home/andytower/Downloads/ --exclude /home/andytower/Yandex.Disk/ --exclude /home/andytower/Загрузки/ /home/andytower/ /backup/ > /backup/backup.log
 cd /backup
 tar -czf /backup/andytower_home.tar.gz home/
